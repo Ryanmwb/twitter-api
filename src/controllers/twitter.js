529dd020ee8131c1a2db5f33d24b22c5e239c1a7
Ryan <ryanmwb@gmail.com>
2019-01-25 18:18:28 -0500

    add blank landing page landing page.

--- src/controllers/twitter.js
+++ src/controllers/twitter.js
@@ -5,14 +5,17 @@ var T = new Twit(twitConfig)
 console.log("app is starting...")
 
 var params = {
-    q: 'rainbow',
+    q: 'trump',
     count: 100
 }
 
 function returnData( err, data, response){
     var tweets = data.statuses;
+    console.log("data is....")
+    console.log(data)
+    console.log("data ended....")
     for(var i=0; i<tweets.length; i++){
-        console.log("Tweet " + [i] + " is......");
+        console.log("Tweet " + [i+1] + " is......");
         console.log(tweets[i].text);
         console.log("");
     }
